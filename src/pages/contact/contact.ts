c8cbfbc1d03791838d1d049fd45b51853fdba9fc
Eduardo Gonzalez <elgs1995@gmail.com>
2018-03-01 01:42:46 -0600

    app terminada

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, IonicPage } from 'ionic-angular';
+import { Storage } from '@ionic/storage'
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
-
+  city:string;
+  state:string;
+  constructor(public navCtrl: NavController, private storage:Storage) {
+    this.storage.get('location').then((val) =>{
+        if(val != null){
+          let location = JSON.parse(val);
+          this.city = location.city;
+          this.state = location.state;
+        } else {
+          this.city = 'San Francisco';
+          this.state = 'CA';
+        }
+    });
   }
 
-}
+} 
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+  apiKey = 'd7ea932d5788108e';
+  url;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json');
+  }
+}
