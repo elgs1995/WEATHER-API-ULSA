b11f540285c29a0ecdfaa5ed9776b0ede50e10bd
Eduardo Gonzalez <elgs1995@gmail.com>
2018-03-01 14:19:24 -0600

    Commit Final

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, IonicPage } from 'ionic-angular';
-import { Storage } from '@ionic/storage'
+import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-  city:string;
-  state:string;
-  constructor(public navCtrl: NavController, private storage:Storage) {
-    this.storage.get('location').then((val) =>{
-        if(val != null){
-          let location = JSON.parse(val);
-          this.city = location.city;
-          this.state = location.state;
-        } else {
-          this.city = 'San Francisco';
-          this.state = 'CA';
-        }
-    });
+
+  constructor(public navCtrl: NavController) {
+
   }
 
 } 
